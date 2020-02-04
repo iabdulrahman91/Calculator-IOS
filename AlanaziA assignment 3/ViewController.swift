f9a9c5e96b753d02d14f5c7bc2dccae77805c9e5
iabdulrahman91 <aalana10@mail.depaul.edu>
2020-02-04 23:55:18 +0300

    update

--- AlanaziA assignment 3/ViewController.swift
+++ AlanaziA assignment 3/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    var pre : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func enterNumber(_ sender: UIButton) {
+        var i:Int = 0
+        if let n = resultLabel.text {
+            i = Int(n)!
+        }
+        
+        var entered:Int = 0
+        if let n = sender.titleLabel?.text {
+            entered = Int(n)!
+        }
+        
+        let removeZero:Int! = Int("\(i.description)\(entered.description)")
+        self.resultLabel.text = "\(removeZero.description)"
+       
+    }
+    
+    @IBAction func addAction(_ sender: Any) {
+        // copy what is been in label and store it into pre
+        var i:Int = 0
+        if let n = resultLabel.text {
+            i = Int(n) ?? 0
+        }
+        self.pre = i
+        // reset label to zero
+        self.resultLabel.text = "0"
+        
+    }
+    @IBAction func calculate(_ sender: Any) {
+        var i = 0
+        if let n = resultLabel.text {
+            i = Int(n) ?? 0
+        }
+        let res = i + self.pre
+        self.pre = 0
+        self.resultLabel.text = "\(res)"
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        self.pre = 0
+        self.resultLabel.text = 0.description
+    }
 }
+
 
